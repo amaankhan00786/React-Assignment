fecffa4cbf337b913fa3448696fd649501eddb51
amaankhan00786 <128613301+amaankhan00786@users.noreply.github.com>
2023-12-15 18:17:19 +0530

    Add files via upload

---
+++ Assignment-walkthrough with React/Q-1/src/App.jsx
@@ -0,0 +1,13 @@
+import {} from 'react'
+import './App.css'
+import Person from './Components/Person';
+
+function App() {
+  return (
+    <>
+      <Person name='Rishabh' age = '18'/>
+    </>
+  )
+}
+
+export default App;
